c6c2b16d8d4d2238b0648e8db011cb6c7c6d33ea
pankaj-2503 <pankajpj3103@gmail.com>
2024-10-01 18:50:33 +0530

    changes1

--- Frontend/taskManager/src/App.jsx
+++ Frontend/taskManager/src/App.jsx
@@ -8,6 +8,7 @@ import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 const routes=(
    <Router>
      <Routes>
+       <Route path="/" element={<Signup/>}></Route>
        <Route path="/dashboard" element={<Home/>}></Route>
        <Route path="/login" element={<Login/>}></Route>
        <Route path="/signup" element={<Signup/>}></Route>
